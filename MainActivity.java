2650fe234d2eb7b14d6602db8dae0c8207d395f4
RHoltje <44952480+RHoltje@users.noreply.github.com>
2019-09-16 21:57:08 -0400

    Add files via upload
    this is the app after watching the second video
    
    string text put in strings.xml instead of straight text in activity_main.xml

--- MainActivity.java
+++ MainActivity.java
@@ -14,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//this is one of the confusing things for me, the xml and java can both alter the appearance and
+// contradict each other.  the button text and color is set one way here, but set differently in the xml file
+// looks like the java wins the argument
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
-        btnSubmit.setText("Play Game");
+        btnSubmit.setText(R.string.button_name);
         btnSubmit.setTextColor(Color.RED);
     }
 }
