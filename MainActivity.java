b4cf9b4826fbd467b702f4b9f33f576a382b2584
RHoltje <44952480+RHoltje@users.noreply.github.com>
2019-09-18 11:25:02 -0400

    changes made during 3rd video

--- MainActivity.java
+++ MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnSubmit;
+    private TextView txtView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,32 @@ public class MainActivity extends AppCompatActivity {
 // contradict each other.  the button text and color is set one way here, but set differently in the xml file
 // looks like the java wins the argument
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        txtView1 = (TextView) findViewById(R.id.txtView1);
+
         btnSubmit.setText(R.string.button_name);
         btnSubmit.setTextColor(Color.RED);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                txtView1.setVisibility(View.VISIBLE);
+                txtView1.setText(R.string.show_text);
+
+        }
+
+    });
+
     }
+
+
+/*
+    public void ShowMeText(View view){
+        //first action on button click
+        txtView1.setVisibility(View.VISIBLE);
+
+        //text is shown
+        txtView1.setText(R.string.show_text);
+    }
+*/
+
 }
