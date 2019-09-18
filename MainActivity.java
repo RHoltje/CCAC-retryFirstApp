016ca7b8ae17a9a8254fb7684c1b5c0a23bad523
RHoltje <44952480+RHoltje@users.noreply.github.com>
2019-09-18 14:54:59 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -6,12 +6,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //declare objects
     private Button btnSubmit;
     private TextView txtView1;
+    private EditText aEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
 //this is one of the confusing things for me, the xml and java can both alter the appearance and
 // contradict each other.  the button text and color is set one way here, but set differently in the xml file
 // looks like the java wins the argument
+
+        //locate the property and assign the value
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
         txtView1 = (TextView) findViewById(R.id.txtView1);
+        aEditText = (EditText) findViewById(R.id.aEditText);
 
         btnSubmit.setText(R.string.button_name);
         btnSubmit.setTextColor(Color.RED);
@@ -29,8 +36,12 @@ public class MainActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View v){
-                txtView1.setVisibility(View.VISIBLE);
-                txtView1.setText(R.string.show_text);
+
+                        String InputText;
+                        InputText = aEditText.getText().toString();
+
+                        txtView1.setVisibility(View.VISIBLE);
+                        txtView1.setText(InputText);
 
         }
 
